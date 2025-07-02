d4fede1322acc9c36b06e17c04f66f6f825101a9
Biel <gkortschinski@gmail.com>
2025-07-02 09:07:29 -0300

    Dependência Lombok instalada e Lombok aplicado a UserEntity

--- src/main/java/com/example/demo/entities/User.java
+++ src/main/java/com/example/demo/entities/User.java
@@ -3,10 +3,15 @@ package com.example.demo.entities;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.time.Instant;
-import java.util.UUID;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "user_tb")
 public class User {
@@ -28,64 +33,4 @@ public class User {
 
     @UpdateTimestamp
     private Instant modificationTimeStamp;
-
-    public User() {
-    }
-
-    public User(Integer userId, String username, String email, String password, Instant creationTimeStamp, Instant modificationTimeStamp) {
-        this.userId = userId;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.creationTimeStamp = creationTimeStamp;
-        this.modificationTimeStamp = modificationTimeStamp;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Instant getCreationTimeStamp() {
-        return creationTimeStamp;
-    }
-
-    public void setCreationTimeStamp(Instant creationTimeStamp) {
-        this.creationTimeStamp = creationTimeStamp;
-    }
-
-    public Instant getModificationTimeStamp() {
-        return modificationTimeStamp;
-    }
-
-    public void setModificationTimeStamp(Instant modificationTimeStamp) {
-        this.modificationTimeStamp = modificationTimeStamp;
-    }
 }
